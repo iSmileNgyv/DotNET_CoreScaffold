375c0cd651f4bb529652d5db77622b6f95d72e6d
ismile <ismayilnagiyev100@gmail.com>
2025-09-12 23:40:37 +0400

    changed ocelot to yarp

--- Gateways/Gateways.APIGateway/Program.cs
+++ Gateways/Gateways.APIGateway/Program.cs
@@ -1,19 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app.UseRouting();
+app.MapReverseProxy();
 app.UseHttpsRedirection();
-
 app.Run();
